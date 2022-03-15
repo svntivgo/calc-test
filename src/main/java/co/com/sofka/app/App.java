76e56953f5199131163be85aef94cde4ff412281
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-15 17:59:49 -0500

    main method at App

--- src/main/java/co/com/sofka/app/App.java
+++ src/main/java/co/com/sofka/app/App.java
@@ -1,13 +1,35 @@
 package co.com.sofka.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import co.com.sofka.app.calculator.BasicCalculator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger( App.class );
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( System.in ) );
+
+        System.out.println( "Enter number 1: " );
+        String textNumber1 = bufferedReader.readLine();
+
+        System.out.println( "Enter number 2: " );
+        String textNumber2 = bufferedReader.readLine();
+
+        Long number1 = Long.valueOf( textNumber1 );
+        Long number2 = Long.valueOf( textNumber2 );
+
+        BasicCalculator calculator = new BasicCalculator();
+        Long result = calculator.suma( number1, number2 );
+
+        System.out.println( number1 + " + " + number2 + " = " + result );
     }
 }
+
--- src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
@@ -6,8 +6,23 @@ import org.slf4j.LoggerFactory;
 public class BasicCalculator {
     private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
 
-    public Long sum(Long number1, Long number2) {
+    public Long suma(Long number1, Long number2) {
         logger.info( "Summing {} + {}", number1, number2 );
         return number1 + number2;
+    }
+
+    public Long resta(Long number1, Long number2) {
+        logger.info( "Summing {} - {}", number1, number2 );
+        return number1 - number2;
+    }
+
+    public Long mult(Long number1, Long number2) {
+        logger.info( "Summing {} * {}", number1, number2 );
+        return number1 * number2;
+    }
+
+    public Long divi(Long number1, Long number2) {
+        logger.info( "Summing {} / {}", number1, number2 );
+        return number1 / number2;
     }
 }
