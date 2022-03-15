1f72e5949810fc8a9d69dd69cbce18f4b1af45e6
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-15 17:50:43 -0500

    BasicCalculator at calculator package

---
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
@@ -0,0 +1,8 @@
+package co.com.sofka.app.calculator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BasicCalculator {
+    private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+}
