1c292cf7a4ae28d44b3b9cf14b6abc9c87f54f7a
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-15 17:52:01 -0500

    sum method at BasicCalculator

--- src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
+++ src/main/java/co/com/sofka/app/calculator/BasicCalculator.java
@@ -5,4 +5,9 @@ import org.slf4j.LoggerFactory;
 
 public class BasicCalculator {
     private static final Logger logger = LoggerFactory.getLogger(BasicCalculator.class);
+
+    public Long sum(Long number1, Long number2) {
+        logger.info( "Summing {} + {}", number1, number2 );
+        return number1 + number2;
+    }
 }
