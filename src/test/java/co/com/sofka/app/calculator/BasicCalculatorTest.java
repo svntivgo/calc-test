5c76f77c0c988300aaf8bec0b555d2dbe810d8a1
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-16 00:59:35 -0500

    test for suma method

---
+++ src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
@@ -0,0 +1,43 @@
+package co.com.sofka.app.calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class BasicCalculatorTest {
+    private final BasicCalculator basicCalculator = new BasicCalculator();
+
+    @Test
+    @DisplayName("Testing suma: 1+1=2")
+    public void sum() {
+        // Arrange
+        Long number1 = 1L;
+        Long number2 = 1L;
+        Long expectedValue = 2L;
+
+        // Act
+        Long result = basicCalculator.suma(number1, number2);
+
+        // Assert
+        assertEquals(expectedValue, result);
+    }
+
+    @DisplayName("Testing several sums")
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    public void severalSums(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.suma(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
+
+
+}
